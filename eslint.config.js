37ccc8cd5322f1348613ee318887d5ba0b8ba2e8
betaHi <laymanz@163.com>
2026-04-15 18:02:53 +0800

    update version to 0.7.3

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,14 @@
 import config from "@echristian/eslint-config"
+import markdown from "@eslint/markdown"
+import { defineConfig } from "eslint/config"
 
-export default config({
+const baseConfig = config({
   prettier: {
     plugins: ["prettier-plugin-packagejson"],
   },
-})
+}).map((entry) => ({
+  ...entry,
+  ignores: [...(entry.ignores ?? []), "**/*.md"],
+}))
+
+export default defineConfig([...baseConfig, ...markdown.configs.recommended])
